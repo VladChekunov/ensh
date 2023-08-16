3b58b883627fce5a45ed69025edabce7c25f0e56
Vladlen Chekunov <chekunov@national.shitposting.agency>
2023-08-16 21:38:07 +0300

    Test to check tg parameters.

--- src/layout/Phrase/index.jsx
+++ src/layout/Phrase/index.jsx
@@ -5,8 +5,11 @@ export function Phrase() {
 	const phrase = location.query.p ?? "London is the capital city of the United Kingdom.";
 	const words = phrase.split(' ');
 
+	const parameters = JSON.stringify(location.query);
+
 	return (
         <div class="wrapper">
+			{ parameters }
 			{
 				words.map((word, index) => (
 					<Word word={word}></Word>
